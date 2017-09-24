bfe9e030f2e63b1169fa0705a8ab8ae09c656574
Edgar Marukyan <emarukyan@gmail.com>
2017-09-24 23:06:58 +0400

    Add some configs and phone-call lib

---
+++ phone-call.js
@@ -0,0 +1,15 @@
+// SDK Version: 2.x 3.x
+// Download the Node helper library from twilio.com/docs/node/install
+// These vars are your accountSid and authToken from twilio.com/user/account
+const config = require('./config')
+const accountSid = config.twilio.accountSid
+const authToken = config.twilio.authToken
+const client = require('twilio')(accountSid, authToken)
+
+client.calls.create({
+  url: 'http://demo.twilio.com/docs/voice.xml',
+  to: config.phoneNumber,
+  from: config.twilio.phoneNumber
+}, function (err, call) {
+  console.log(call.sid)
+})
