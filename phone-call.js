f0a642e38aa4d9c0731216e8892b8ed6c47eba1b
Edd Marukyan <emarukyan@gmail.com>
2017-10-14 17:50:44 +0400

    add ability to call multiple numbers

--- phone-call.js
+++ phone-call.js
@@ -9,7 +9,11 @@ const client = require('twilio')(accountSid, authToken)
 module.exports = (message, cb) => {
   const numbers = []
   const confNumb = config.phoneNumber
-  numbers.push(confNumb.indexOf(',') !== -1 ? confNumb.split(',') : confNumb)
+  if (confNumb.includes(',')) {
+    numbers.push(...confNumb.split(','))
+  } else {
+    numbers.push(confNumb)
+  }
 
   numbers.map(number => {
     console.log(`Calling to ${number}`)
